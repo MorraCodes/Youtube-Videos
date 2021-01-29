e851ff5baba46414f7882ef93f539cd7b314ca6e
MorraCodes <omarmoh.said@yahoo.com>
2021-01-28 20:09:16 -0500

    Update bubble_sort.py
    Track this variable: isSorted

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,25 +1,24 @@
 """
-Created on Thu Jan 28 18:28:46 2021
-@author: MorraCodes
+Created on Tue Jan 26 19:43:50 2021
+
+@author: omarm
 """
 
-array = [9,5,3,2,1,100,55,22,11,10] # this unsorted array
+array = [9,5,3,2,1,100,55,22,11,10]
+
 
 def bubble_sort(array):
-    isSorted = False # unsorted array boolean
-    
-    while not isSorted: # as long as it is not sorted keep going / sorting
-        isSorted = True # sorted
-        loop_less = 0
-        for i in range(len(array) - 1 - loop_less):
-            if array[i] >  array[i+1]:
-                array[i], array[i+1] = array[i+1],array[i] # swap procedure
-                isSorted = False # unsorted
-            loop_less += 1
+    isSorted = False
+    loop_less = 0
+    while not isSorted:
+        isSorted = True
+        for i in range( len(array) - 1 - loop_less):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1] , array[i]
+                isSorted = False
+        loop_less += 1
     return array
                 
-                
-            
 
-sorted_array = bubble_sort(array) # sorted array
+sorted_array = bubble_sort(array)
 print(sorted_array)
